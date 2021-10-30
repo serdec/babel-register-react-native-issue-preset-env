d7728bf8a722aa17a136346a0f5f0974821ebb06
Sergio de Cristofaro <sdecri@gmail.com>
2021-10-30 08:32:57 +0200

    added react native tests

--- app.test.js
+++ app.test.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import {describe} from 'riteway';
 import {View} from 'react-native';
+import {render} from '@testing-library/react-native';
+
+describe('test', async assert => {
+  const {queryAllByTestId} = render(<View testID='home'/>)
+  assert({
+    given: 'no arguments',
+    should: 'compare 1 and 1',
+    actual: 1,
+    expected: queryAllByTestId('home').length,
+  })
+})
--- babel-register-start.js
+++ babel-register-start.js
@@ -1,4 +1,5 @@
 require('@babel/register')({
   ignore: [/node_modules\/(?!(react-native))/],
-  presets: ['@babel/preset-env'],
+  presets: [['@babel/preset-env', {loose: true}], '@babel/preset-flow', '@babel/preset-react', ["module:metro-react-native-babel-preset", { "loose": true }]],
 });
+global.__DEV__ = true;
